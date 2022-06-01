a3a8ce4af2d3567aaef0e0376614e5aa1413b667
sbashevkin <sam.bashevkin@deltacouncil.ca.gov>
2022-06-01 11:59:10 -0700

    Adding `deltafish` to universe

--- create_json.R
+++ create_json.R
@@ -1,7 +1,8 @@
 df <- data.frame(
-  package = c("discretewq", "LTMRdata", "deltareportr", "spacetools", "zooper", "deltamapr"),
+  package = c("discretewq", "LTMRdata", "deltareportr", "spacetools", "zooper", "deltamapr", "deltafish"),
   url = c("https://github.com/sbashevkin/discretewq", "https://github.com/sbashevkin/LTMRdata", 
           "https://github.com/sbashevkin/deltareportr", "https://github.com/sbashevkin/spacetools", 
-          "https://github.com/InteragencyEcologicalProgram/zooper", "https://github.com/InteragencyEcologicalProgram/deltamapr")
+          "https://github.com/InteragencyEcologicalProgram/zooper", "https://github.com/InteragencyEcologicalProgram/deltamapr", 
+          "https://github.com/delta-stewardship-council/deltafish")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
